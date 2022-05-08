d2e97308fadc363d002c0ac9c0e7c90b132d2000
Daniel Leiva <droguier@users.noreply.github.com>
2022-05-08 01:55:28 -0400

    test 5
    rtetset

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -2,35 +2,30 @@ var store = [{
         "title": "Test Recipe",
         "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
         "tags": [],
-        "url": "https://droguier.cl//droguier/test-chip-cookies/"
+        "url": "/droguier/test-chip-cookies/"
       },{
         "title": "Test Recipe 1",
         "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
         "tags": [],
-        "url": "https://droguier.cl//florecerterapias/galeria/gallery-chip-cookies_v1"
+        "url": "/florecerterapias/galeria/gallery-chip-cookies_v1"
       },{
         "title": "Video: El universo en una copa de vino, por Richard Feynman",
         "excerpt":"Hermoso poema que relaciona de una forma natural y simple porque las ciencias son necesarias para poder disfrutar un poquito más de las cosas simples   ","categories": ["Post Formats"],
         "tags": ["Post Formats"],
-        "url": "https://droguier.cl//florecerterapias/galeria/gallery-video-poem"
+        "url": "/florecerterapias/galeria/gallery-video-poem"
       },{
         "title": "Test Recipe 1",
         "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
         "tags": [],
-        "url": "https://droguier.cl//florecerterapias/publicaciones/test-chip-cookies_v1"
+        "url": "/florecerterapias/publicaciones/test-chip-cookies_v1"
       },{
         "title": "Test Recipe 2",
         "excerpt":"Oliwi . A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of...","categories": [],
         "tags": [],
-        "url": "https://droguier.cl//florecerterapias/publicaciones/test-chip-cookies_v2"
+        "url": "/florecerterapias/publicaciones/test-chip-cookies_v2"
       },{
         "title": "Test Recipe 3",
         "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
         "tags": [],
-        "url": "https://droguier.cl//florecerterapias/publicaciones/test-chip-cookies_v3"
-      },{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "https://droguier.cl//jekyll/update/2022/04/11/welcome-to-jekyll.html"
-      }]
+        "url": "/florecerterapias/publicaciones/test-chip-cookies_v3"
+      },]
