fa33e5f2a6bf52410f1728cc3c28ff0865fe28bb
Daniel Leiva <droguier@users.noreply.github.com>
2022-05-22 23:29:48 -0400

    BASE
    Formato base para empezar a desarrollar

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,11 +1,16 @@
 var store = [{
+        "title": "Video: El universo en una copa de vino, por Richard Feynman",
+        "excerpt":"Hermoso poema que relaciona de una forma natural y simple porque las ciencias son necesarias para poder disfrutar un poquito más de las cosas simples   ","categories": ["Post Formats"],
+        "tags": ["Post Formats"],
+        "url": "https://droguier.cl/droguier/gallery-video-poem"
+      },{
         "title": "Test Recipe",
         "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
         "tags": [],
-        "url": "https://droguier.cl//droguier/test-chip-cookies/"
+        "url": "https://droguier.cl/droguier/test-chip-cookies"
       },{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "https://droguier.cl//jekyll/update/2022/04/11/welcome-to-jekyll.html"
+        "url": "https://droguier.cl/jekyll/update/2022/04/11/welcome-to-jekyll.html"
       }]
